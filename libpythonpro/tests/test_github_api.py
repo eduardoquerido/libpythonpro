4c4c7f3aceb2ceb855836eb084194090b5222b45
Eduardo Querido <eduardo.querido@znc.com.br>
2019-09-25 14:39:04 -0300

    Utilizando a biblioteca mocker(pytest-mock) para alterar uma biblioteca em seu teste sem que haja interferência em outros testes que utilizam a mesma biblioteca

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -7,6 +7,8 @@ from libpythonpro import github_api
 Criando um objeto mock para testar o acesso a api do github, sem precisar de fato acessá-lo, caso venha a ocorrer problemas,
 como: queda de internet, queda de servidor da api e entre outros
 '''
+
+'''
 @pytest.fixture
 def avatar_url():
     resp_mock = Mock()
@@ -19,6 +21,22 @@ def avatar_url():
     github_api.requests.get = Mock(return_value=resp_mock)
     yield url
     github_api.requests.get = get_original
+'''
+
+@pytest.fixture
+def avatar_url(mocker):
+    '''
+    Utilizando a biblioteca mocker você pode trocar a biblioteca por uma objeto mock sem interferir em outros testes
+    '''
+    resp_mock = Mock()
+    url = 'https://avatars0.githubusercontent.com/u/50111507?v=4'
+    resp_mock.json.return_value ={
+        'login': 'eduardoquerido', 'id': 50111507,
+        'avatar_url': url,
+    }
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 
